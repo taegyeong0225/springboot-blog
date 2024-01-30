062405fdb170fff5d613d6b65d0decdf48debf55
고태경 <202244042@itc.ac.kr>
2024-01-31 02:01:33 +0900

    게시글 삭제 기능 추가

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -72,5 +73,19 @@ public class MemberController {
         }
 
         return "redirect/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String edit(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다!");
+        }
+        return "redirect:/articles";
     }
 }
