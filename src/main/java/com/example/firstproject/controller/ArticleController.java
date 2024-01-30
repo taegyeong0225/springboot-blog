b819f9c9be6e1b75246819dabefc34ba9f3e5a97
고태경 <202244042@itc.ac.kr>
2024-01-31 01:30:39 +0900

    게시글 수정 기능 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,18 +6,24 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
     }
+
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
 
@@ -29,6 +35,49 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 
-        return "";
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id= " + id);
+        // 1. id를 조회해 데이터 가져오기 (Optinal<Article>)
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        return "redirect/articles/" + articleEntity.getId();
     }
 }
--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,20 +1,26 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
-    @GetMapping("/sighup")
+    @GetMapping("/signup")
     public String newMemberForm() {
         return "/members/new";
     }
@@ -28,6 +34,43 @@ public class MemberController {
 
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
-        return "";
+        return "redirect:/members/" + saved.getId();
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members/index";
+    }
+
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) {
+        log.info(form.toString());
+
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        if (target != null) {
+            memberRepository.save(memberEntity);
+        }
+
+        return "redirect/members/" + memberEntity.getId();
     }
 }
