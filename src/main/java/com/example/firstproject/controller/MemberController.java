bc481517ed1f28c928a0dda6a99ef06f9aa95e11
고태경 <202244042@itc.ac.kr>
2024-01-30 20:32:14 +0900

    롬복 리팩터링 완료

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+    @GetMapping("/sighup")
+    public String newMemberForm() {
+        return "/members/new";
+    }
+
+    @PostMapping("/join")
+    public String createMember(MemberForm memberForm) {
+        log.info(memberForm.toString());
+
+        Member member = memberForm.toEntity();
+        log.info(member.toString());
+
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
+        return "";
+    }
+}
---
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    Long id;
+    @Column
+    String email;
+    @Column
+    String password;
+}
